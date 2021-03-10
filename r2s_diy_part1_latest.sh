26283e316f215aee425a550f3fafc251240a695c
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-10 21:07:24 +0800

    Update r2s_diy_part1_latest.sh

--- r2s_diy_part1_latest.sh
+++ r2s_diy_part1_latest.sh
@@ -16,3 +16,10 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default  
+
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/lienol
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr 
