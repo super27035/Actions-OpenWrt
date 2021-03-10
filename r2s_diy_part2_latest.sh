e3cec757b718e9357c8296d868e2c3b987c55c13
super27035 <43180500+super27035@users.noreply.github.com>
2021-03-10 21:12:02 +0800

    Update r2s_diy_part2_latest.sh

--- r2s_diy_part2_latest.sh
+++ r2s_diy_part2_latest.sh
@@ -11,12 +11,15 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate#1. Modify default IP
-#1 IP address
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-#2 password
-sed -i 's/root::0:0:99999:7:::/root:$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-#3. Modify default hosename
+#2. Modify default hosename
 sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
+
+#3. Change the login password
+# $1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7::: #superlyn
+# $1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::     #password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
